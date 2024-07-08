a091bff293a326809d5a709e6b45560812841af5
Omansh Kumar <omanshkr@gmail.com>
2024-07-08 18:57:14 +0530

    update

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+@app.route('/',methods=['GET'])
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
